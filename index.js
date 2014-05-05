5f83a759afe19e26cf78c3375b4ac81e1b73172e
Dan MacTough <danmactough@gmail.com>
2014-05-05 12:33:36 -0400

    Reimplement onConnect method as a command queue

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var modeler = require('modeler');
+var modeler = require('modeler')
+  , microtime = require('microtime');
 
 module.exports = function (_opts) {
   var api = modeler(_opts)
@@ -11,6 +12,9 @@ module.exports = function (_opts) {
     , nano = api.options.db
     , name = api.options.dbPrefix + '$' + api.options.name;
 
+  // for the command queue
+  var q = [], blocking = false;
+
   function continuable (skip, limit, reverse, cb) {
     (function next () {
       var options = {};
@@ -19,12 +23,13 @@ module.exports = function (_opts) {
       if (limit) {
         options.limit = limit;
       }
-      onConnect('list', options, function (err, response) {
+      q.push(['list', options, function (err, response) {
         if (err) return cb(err);
         var results = response.rows.map(function (row) { return row.id; });
         skip += results.length;
         cb(null, results, next);
-      });
+      }]);
+      onConnect();
     })();
   }
 
@@ -69,24 +74,27 @@ module.exports = function (_opts) {
     continuable(skip, limit, true, cb);
   };
   api._save = function (entity, cb) {
-    onConnect('insert', api._beforeSave(entity), entity.id, function (err, response) {
+    entity.__idx || (entity.__idx = microtime.now());
+    q.push(['insert', api._beforeSave(entity), entity.id, function (err, response) {
       if (err) return cb(err);
       cb(null, api._afterSave(response));
-    });
+    }]);
+    onConnect();
   };
   api._load = function (id, cb) {
-    onConnect('get', id, function (err, entity) {
+    q.push(['get', id, function (err, entity) {
       if (err) {
         if (nonErrors.indexOf(err.message) === -1) return cb(err);
         else return cb();
       }
       cb(err, api._afterLoad(entity));
-    });
+    }]);
+    onConnect();
   };
   api._destroy = function (id, cb) {
     api._load(id, function (err, entity) {
       if (err) return cb(err);
-      onConnect('destroy', id, entity._rev, cb);
+      db.destroy(id, entity._rev, cb);
     });
   };
 
@@ -96,16 +104,28 @@ module.exports = function (_opts) {
   // so every collection of documents must be in its own database. But,
   // we want them namespaced so that two applications can have collections
   // with the same name.
+
+  // Implemented as command queue to ensure execution order of async operations
+  // Lifted and adapted from https://github.com/carlos8f/modeler-leveldb
   function onConnect () {
-    var args = [].slice.call(arguments);
+    if (blocking || !q.length) return;
+
+    function unblock () {
+      blocking = false;
+      (typeof setImmediate === 'function' ? setImmediate : process.nextTick)(onConnect);
+    }
+
+    blocking = true;
+    var args = q.shift();
     var method = args.shift();
-    var cb = args[args.length - 1];
+    var cb = args.pop();
+    args.push(onConnected);
 
     if (db) return doDb();
     nano.db.get(name, function (err) {
       if (err) {
         nano.db.create(name, function (err) {
-          if (err) return cb(err);
+          if (err) return onConnected(err);
           doDb(nano.db.use(name));
         });
       }
@@ -115,6 +135,11 @@ module.exports = function (_opts) {
     function doDb (_db) {
       if (_db) db = _db;
       db[method].apply(db, args);
+    }
+
+    function onConnected (err, response) {
+      unblock();
+      cb(err, response);
     }
   }
 };
